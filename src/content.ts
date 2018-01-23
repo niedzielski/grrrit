1a08fb02f0576f01a63e8882409b42d5a7abb3b9
Stephen Niedzielski <stephen@niedzielski.com>
2018-01-23 14:58:51 -0800

    Update: remove need for active tab permission

--- src/content.ts
+++ src/content.ts
@@ -1,37 +1,20 @@
 import {setBackgroundColor, getBackgroundColor, HOST} from './common'
 
-function getCurrentTabUrl(): Promise<string | undefined> {
-  return new Promise(resolve => {
-    const queryInfo = {active: true, currentWindow: true}
-    chrome.tabs.query(queryInfo, tabs => {
-      const url = tabs[0].url
-      const host = url && new URL(url).host
-      resolve(host === HOST ? host : undefined)
-    })
-  })
-}
-
 function saveBackgroundColor(url: string, color: string) {
   chrome.storage.sync.set({[url]: color})
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  getCurrentTabUrl()
-    .then(
-      url => (url ? getBackgroundColor(url).then(color => ({url, color})) : {})
-    )
-    .then(({url, color}: {url?: string; color?: string}) => {
-      const dropDown = <HTMLSelectElement>document.getElementById('dropDown')
-      if (color) {
-        setBackgroundColor(color)
-        dropDown.value = color
-      }
+  getBackgroundColor(HOST).then(color => {
+    const dropDown = <HTMLSelectElement>document.getElementById('dropDown')
+    if (color) {
+      setBackgroundColor(color)
+      dropDown.value = color
+    }
 
-      if (url) {
-        dropDown.addEventListener('change', () => {
-          setBackgroundColor(dropDown.value)
-          saveBackgroundColor(url, dropDown.value)
-        })
-      }
+    dropDown.addEventListener('change', () => {
+      setBackgroundColor(dropDown.value)
+      saveBackgroundColor(HOST, dropDown.value)
     })
+  })
 })
