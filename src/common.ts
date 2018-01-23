b03cc847aa6461d94328a2015963075172ac3912
Stephen Niedzielski <stephen@niedzielski.com>
2018-01-23 14:04:49 -0800

    Chore: replace string with template string

--- src/common.ts
+++ src/common.ts
@@ -2,15 +2,14 @@ export function getSavedBackgroundColor(
   url: string,
   callback: (color: string) => void
 ) {
-  chrome.storage.sync.get(url, items => {
+  chrome.storage.sync.get(url, items =>
     callback(chrome.runtime.lastError ? undefined : items[url])
-  })
+  )
 }
 
 export function changeBackgroundColor(color: string) {
-  const code =
-    'document.body.style.setProperty("background-color", "' +
-    color +
-    '", "important");'
+  const code = `
+    document.body.style.setProperty('background-color', '${color}', 'important')
+  `
   chrome.tabs.executeScript({code})
 }
